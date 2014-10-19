28db6ca06891df95a73db9594a6832ee72235c09
Kasim Te <kasimte@gmail.com>
2014-10-19 17:59:55 -0400

    Got a histogram up.

--- server.R
+++ server.R
@@ -8,6 +8,14 @@ shinyServer(
         
         dataset <- rock
         
+        output$main_plot <- renderPlot({
+            hist(rock$area,
+                 probability = TRUE,
+                 breaks = as.numeric(input$n_breaks),
+                 xlab = "area of pores space, in pixels out of 256 by 256",
+                 main = "Rock cross-section sample pore area")
+        })
+        
         # Generate a summary of the dataset
         output$summary <- renderPrint({
             summary(dataset)
